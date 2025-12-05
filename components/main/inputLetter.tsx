46fa2580e691361578125f92f13340deadb5bd26
badyan <bogdanwov@gmail.com>
2025-12-06 00:16:13 +0200

    correct h4

--- components/main/inputLetter.tsx
+++ components/main/inputLetter.tsx
@@ -7,7 +7,7 @@ interface InputLetterProps {
 export default function InputLetter({value, handleChange, handleClick}: InputLetterProps){
     return(
         <>
-            <h4>letters with which the password will be created</h4>
+            <h4>your password</h4>
             <div>
                 <input type="text" value ={value} onChange={handleChange} className="rounded-lg p-2 bg-blue-light m-3" />
                 <button onClick={handleClick} className="rounded-lg bg-pink-ligth p-1.5 text-blue-light">
