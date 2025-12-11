8d0c2975c70179aa80ec6a33987c255aba1f8a93
badyan <bogdanwov@gmail.com>
2025-12-11 23:33:08 +0200

    implement genPass func. And always leawing one option

--- components/main/main.tsx
+++ components/main/main.tsx
@@ -7,21 +7,36 @@ import Options from "./options"
 
 export default function Main(){
     const [inputValue, setInputValue] = useState('')
-    const [selctedOptions, setSelectedOptions] = useState<string[]>([])
+    const [selctedOptions, setSelectedOptions] = useState<string[]>(["abcdefghijklmnopqrstuvwxyz"])
+    const [length, setLength] = useState<number>(10)
 
     function toggleOption(option: string){
-        setSelectedOptions( prev =>
-            prev.includes(option)
-            ? prev.filter( o => o !== option)
-            : [...prev, option]
-        )
+        setSelectedOptions(prev => {
+            const isSelected = prev.includes(option) 
+            if (prev.length === 1 && isSelected) return prev // не можна вимкнути всі опції
+            return isSelected
+            ? prev.filter(o => o !== option)
+            : [...prev, option];
+        }
+        );
     }
     function handleChange(event: React.ChangeEvent<HTMLInputElement>){
         setInputValue(event.target.value)
     }
+    function handleLength(event: React.ChangeEvent<HTMLInputElement>){
+        setLength(Number(event.target.value))
+    }
 
-    function genPass(){
-        
+    function genPass(): void{
+        let genData = ''
+        for (let option of selctedOptions){
+            genData += option
+        }
+        let password = ''
+        for (let i = 0; i < length; i++){
+            password += genData[Math.floor(Math.random() * genData.length)];
+        }
+        setInputValue(password)
     }
 
     return(
@@ -30,9 +45,14 @@ export default function Main(){
                 <InputLetter value={inputValue} handleChange={handleChange} handleClick={genPass}/>
             </div>
             <div className="row-span-2 p-20">
-                <Options selctedOptions={selctedOptions} toggleOption={toggleOption}/>
+                <Options
+                selctedOptions={selctedOptions}
+                toggleOption={toggleOption}
+                length={length}
+                handleLenght={handleLength}
+                />
             </div>
-            <div className="h bg-red-very-ligth">
+            <div className="h ">
                 <Result passwords=""/>
             </div>
         </main>
--- components/main/options.tsx
+++ components/main/options.tsx
@@ -3,26 +3,46 @@ import { Interface } from "readline"
 import { buffer } from "stream/consumers"
 
 interface OptionsProp{
-    selctedOptions: string[];
-    toggleOption: (option: string) => void;
+    length: number
+    handleLenght: (event: React.ChangeEvent<HTMLInputElement>) => void
+    selctedOptions: string[]
+    toggleOption: (option: string) => void
 
 }
 
-export default function Options({selctedOptions, toggleOption}: OptionsProp){
-    const allOptions = [ "opt 1", "opt 2", "opt 3"]
+export default function Options({selctedOptions, toggleOption, length, handleLenght}: OptionsProp){
+    const data = [
+        ["numbers", "0123456789"],
+        ["symbols", "!@#$%^&*()_+-=[]{};:,.<>?"],
+        ["uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+        ["lowercase", "abcdefghijklmnopqrstuvwxyz"]
+    ]
 
     return(
         <>
-            {allOptions.map( (option) => (
+            {data.map( (option) => (
                 <button
-                key={option}
-                onClick={ () => toggleOption(option) }
-                className={`${selctedOptions.includes(option) ? "bg-pink-ligth" : "bg-pink-600"}
-                    min-w-14 min-h-14 border-black border m-2 text-blue-light rounded-lg`}
-                >
-                    { option }
+                    key={option[0]}
+                    onClick={ () => toggleOption(option[1]) }
+                    className={`${selctedOptions.includes(option[1]) ? "bg-pink-ligth" : "bg-pink-600"}
+                        min-w-14 min-h-14 border-black border m-2 text-blue-light rounded-lg`}
+                    >
+                    { option[0] }
                 </button>
             ))}
+            <div>
+                <label htmlFor="length">
+                    {length}
+                </label>
+                <input
+                    type="range"
+                    id="length"
+                    min="4"
+                    max="32"
+                    value={length}
+                    onChange={(e) => handleLenght(e)}
+                />
+            </div>
         </>
     )
 }
